f99be0a3834752652e80730f26dd5749d26c6bbd
Ruslan_Bekmatov <bekmatov_ruslan@mail.ru>
2022-02-04 17:16:40 +0400

    Тест 1

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,18 +1,48 @@
 package tests;
 
 import com.google.common.annotations.VisibleForTesting;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class googleTests {
 
-@Test
-   public void test1(){
-   System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-   WebDriver driver = new ChromeDriver();
-   driver.get("http://google.com");
-   driver.quit();
+   private static WebDriver driver;
+
+   @BeforeAll
+   public static void init(){
+      System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+      ChromeOptions options = new ChromeOptions();
+      options.addArguments("start-maximized");
+      driver = new ChromeDriver(options);
    }
+@Test
+   public void test1() {
+   driver.get("http://google.com");
+   driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+   driver.findElement(By.xpath("//div[.='(']")).click();
+   driver.findElement(By.xpath("//div[.='1']")).click();
+   driver.findElement(By.xpath("//div[.='+']")).click();
+   driver.findElement(By.xpath("//div[.='2']")).click();
+   driver.findElement(By.xpath("//div[.=')']")).click();
+   driver.findElement(By.xpath("//div[.='×']")).click();
+   driver.findElement(By.xpath("//div[.='3']")).click();
+   driver.findElement(By.xpath("//div[.='−']")).click();
+   driver.findElement(By.xpath("//div[.='4']")).click();
+   driver.findElement(By.xpath("//div[.='0']")).click();
+   driver.findElement(By.xpath("//div[.='÷']")).click();
+   driver.findElement(By.xpath("//div[.='5']")).click();
+   driver.findElement(By.xpath("//div[.='=']")).click();
+
+}
+   @AfterAll
+   public static void teardown() {
+      ;
+}
 
 }
