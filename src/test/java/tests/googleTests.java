3f20a7e401a9e583afa5bf2bde1fa865e39c515f
Ruslan_Bekmatov <bekmatov_ruslan@mail.ru>
2022-02-05 21:22:32 +0400

    Тест 1

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -2,15 +2,22 @@ package tests;
 
 import com.google.common.annotations.VisibleForTesting;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class googleTests {
+import javax.swing.text.html.parser.Element;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class googletests {
 
    private static WebDriver driver;
 
@@ -38,11 +45,33 @@ public class googleTests {
    driver.findElement(By.xpath("//div[.='÷']")).click();
    driver.findElement(By.xpath("//div[.='5']")).click();
    driver.findElement(By.xpath("//div[.='=']")).click();
+   assertEquals("1", driver.findElement(By.cssSelector("#cwos")).getText());
+   assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div[1]/div/div/div[1]/div[2]/div[1]/div/span")).getText());
+
+
 
 }
-   @AfterAll
+@Test
+   public void test2() {
+   driver.get("http://google.com");
+   driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+   driver.findElement(By.xpath("//div[.='6']")).click();
+   driver.findElement(By.xpath("//div[.='÷']")).click();
+   driver.findElement(By.xpath("//div[.='0']")).click();
+   driver.findElement(By.xpath("//div[.='=']")).click();
+   assertEquals("Infinity", driver.findElement(By.cssSelector("#cwos")).getText());;
+}
+   @Test
+public void test3() {
+      driver.get("http://google.com");
+      driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+      driver.findElement(By.xpath("//div[.='sin']")).click();
+      driver.findElement(By.xpath("//div[.='=']")).click();
+
+   }
+@AfterAll
    public static void teardown() {
-      ;
+driver.quit();
 }
 
 }
